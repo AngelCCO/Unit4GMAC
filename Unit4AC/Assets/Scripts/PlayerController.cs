4b740de61984382fe27ef6faee0ece71ed1389bc
AngelCCO <116102695+AngelCCO@users.noreply.github.com>
2023-09-11 11:37:28 -0700

    Unit 4 is finished
    Finished

--- Unit4AC/Assets/Scripts/PlayerController.cs
+++ Unit4AC/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,14 @@ public class PlayerController : MonoBehaviour
     private GameObject tmpRocket;
     private Coroutine powerUpCountdown;
 
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+
+    bool smashing = false;
+    float floorY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +44,13 @@ public class PlayerController : MonoBehaviour
         if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
         {
             LaunchRockets();
+        }
+
+        if(currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            
+            smashing = true;
+            StartCoroutine(Smash());
         }
     }
 
@@ -84,5 +99,35 @@ public class PlayerController : MonoBehaviour
             tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
             tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
         }
+    }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+
+        floorY = transform.position.y;
+
+        float jumpTime = Time.time + hangTime;
+
+        while(Time.time < jumpTime)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+
+        smashing = false;
+        
     }
 }
--- Unit4AC/Assets/Scripts/RocketBehaviour.cs
+++ Unit4AC/Assets/Scripts/RocketBehaviour.cs
@@ -25,7 +25,7 @@ public class RocketBehaviour : MonoBehaviour
 
     public void Fire(Transform newTarget)
     {
-        target = homingTarget;
+        target = newTarget;
         homing = true;
         Destroy(gameObject, aliveTimer);
     }
